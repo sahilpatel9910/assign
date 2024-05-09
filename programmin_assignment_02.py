941be552b800611f5756f72a5cc78ffed96b646c
sahilpatel9910 <98449155+sahilpatel9910@users.noreply.github.com>
2024-05-09 03:38:15 +0000

    done'

--- programmin_assignment_02.py
+++ programmin_assignment_02.py
@@ -1,22 +1,3 @@
-print("hello world")
-
-
-class course:
-    def __init__(self,name , marks):
-        self.name=name
-        self.marks=marks
-
-    def display(self):
-        print(self.name, self.marks)
-    
-c1=course("sail",2.3)
-
-c1.display()
-
-print("hello world")
-
-
-
 
 class Customer:
     def __init__(self,ID,name,rewards):
